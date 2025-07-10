7667993abf992f5d130457fee66dd859e1537996
Nikita Shilin <shilin.nick@yandex.ru>
2025-07-10 10:16:58 +0300

    project was created

---
+++ main.py
@@ -0,0 +1,10 @@
+class Student:
+    self.name = name
+    self.surname = 
+    self.age = 10
+
+    def print_ifno():
+        print('Student:')
+        print(self.name)
+        print(self.surname)
+        print(age)
